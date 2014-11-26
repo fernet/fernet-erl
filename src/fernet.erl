a241919a6a476da33429fa8e799dfc5c94608f2e
Kevin McDermott <kevin@heroku.com>
2014-11-26 17:13:28 +0000

    Failing generate_token.

--- src/fernet.erl
+++ src/fernet.erl
@@ -29,12 +29,12 @@ encode_token(Token) ->
 generate_token(Message, Key) ->
   generate_token(Message, generate_iv(), timestamp_to_seconds(now()), Key).
 
-generate_token(_Message, IV, Seconds, Key) ->
+generate_token(Message, IV, Seconds, Key) ->
   Encoded_Seconds = seconds_to_binary(Seconds),
-  Padded = <<104, 101, 108, 108, 111, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11>>,
+  Padded = pkcs7:pad(list_to_binary(Message)),
   Cyphertext = block_encrypt(extract_encryption_key(Key), IV, Padded),
-  _Hmac = generate_hmac(Encoded_Seconds, IV, Cyphertext, extract_signing_key(Key)),
-  encode_token(<<<<?VERSION>>/binary, Encoded_Seconds/binary, IV/binary, Cyphertext/binary>>).
+  Hmac = generate_hmac(Encoded_Seconds, IV, Cyphertext, extract_signing_key(Key)),
+  encode_token(<<<<?VERSION>>/binary, Encoded_Seconds/binary, IV/binary, Cyphertext/binary, Hmac/binary>>).
 
 verify_and_decrypt_token(Token, Key, TTL) ->
   verify_and_decrypt_token(Token, Key, TTL, timestamp_to_seconds(now())).
@@ -158,9 +158,9 @@ extract_signing_key_test() ->
 %]
 % 1985-10-26T01:20:00-07:00 == 499162800 Seconds since the epoch
 %
-%% generate_token_test() ->
-%%   Tok = generate_token("hello", <<0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15>>, 499162800, decode_key("cw_0x689RpI-jtRR7oE8h_eQsKImvJapLeSbXpwF4e4=")),
-%%   ?assertEqual("gAAAAAAdwJ6wAAECAwQFBgcICQoLDA0ODy021cpGVWKZ_eEwCGM4BLLF_5CV9dOPmrhuVUPgJobwOz7JcbmrR64jVmpU4IwqDA==", Tok).
+generate_token_test() ->
+  Tok = generate_token("hello", <<0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15>>, 499162800, decode_key("cw_0x689RpI-jtRR7oE8h_eQsKImvJapLeSbXpwF4e4=")),
+  ?assertEqual("gAAAAAAdwJ6wAAECAwQFBgcICQoLDA0ODy021cpGVWKZ_eEwCGM4BLLF_5CV9dOPmrhuVUPgJobwOz7JcbmrR64jVmpU4IwqDA==", Tok).
 
 generate_hmac_test() ->
   SigningKey = <<115, 15, 244, 199, 175, 61, 70, 146, 62, 142, 212, 81, 238, 129, 60, 135>>,
@@ -210,13 +210,13 @@ extract_encryption_key_test() ->
 %%   }
 %% ]
 
-verify_and_decrypt_token_test() ->
-    Token = "gAAAAAAdwJ6wAAECAwQFBgcICQoLDA0ODy021cpGVWKZ_eEwCGM4BLLF_5CV9dOPmrhuVUPgJobwOz7JcbmrR64jVmpU4IwqDA==",
-    TTL = 60,
-    Secret = "cw_0x689RpI-jtRR7oE8h_eQsKImvJapLeSbXpwF4e4=",
-    Now = 499162800,
-    {ok, Message} = verify_and_decrypt_token(Token, Secret, TTL, Now),
-    ?assertEqual("hello", Message).
+%% verify_and_decrypt_token_test() ->
+%%     Token = "gAAAAAAdwJ6wAAECAwQFBgcICQoLDA0ODy021cpGVWKZ_eEwCGM4BLLF_5CV9dOPmrhuVUPgJobwOz7JcbmrR64jVmpU4IwqDA==",
+%%     TTL = 60,
+%%     Secret = "cw_0x689RpI-jtRR7oE8h_eQsKImvJapLeSbXpwF4e4=",
+%%     Now = 499162800,
+%%     {ok, Message} = verify_and_decrypt_token(Token, Secret, TTL, Now),
+%%     ?assertEqual("hello", Message).
 
 -endif.
 
